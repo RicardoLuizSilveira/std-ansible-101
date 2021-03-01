c1edda60472547c225bb99020c837ab16d782e93
Ricardo Luiz <ricardo.silveira@gmail.com>
2021-03-01 14:13:08 -0300

    Ansible initial commit

---
+++ course_sandbox/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["ffi",[">= 1.0.0"]],["win32-process",[">= 0"]],["vagrant-sshfs",["= 1.3.5"]]],"checksum":"a3c61d3d67a23b8639017d5f6f75d7ef50d0a7c4cdab7682df05d2ffa97a207a","vagrant_version":"2.2.14"}
